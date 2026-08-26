a03ae4ccc82335150ff0a9ac49d4082995da3064
HUISU <133619212+notitle12@users.noreply.github.com>
2026-08-26 16:00:24 +0900

    chore: initialize Spring Boot backend project (pebble-api)

---
+++ src/test/java/com/pebble/api/PebbleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pebble.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PebbleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
